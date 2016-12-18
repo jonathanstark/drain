8cc36cbdc12eea2a5979ef0b45beaa9f70b805cf
Jonathan Stark <jstark@jonathanstark.com>
2016-12-18 15:13:56 -0500

    initial commit

---
+++ config/private-sample.php
@@ -0,0 +1,9 @@
+<?php
+define('API_KEY', 'asdf1234asdf1234asdf'); // You can find this on your user page in drip: https://www.getdrip.com/user/edit
+define('ACCOUNT_ID', '1234567'); // You can find this in your drip url ala: https://jstark.co/2gPfFP5
+define('APP_NAME', 'MyAppName'); // An arbitrary name you make up
+define('STATUS_FILTER', 'sent'); // Possible values: draft, scheduled, sent, all
+define('MAX_ITERATIONS', 5); // Avoid infinite loop, just in case (increase this is you have more than 500 broadcasts)
+define('DELETE_EVERYTHING_AFTER_STRING', '{%'); // A string found near the end of each message used to trim off everything after
+define('PAGE_FOOTER', "----\n\n[« Back to home](https://mysite.com/)"); // Arbitrary content that will be appended to the end of each file
+define('APPEND_SUBJECT_TO_FILENAME', true); //
---
+++ html/index.php
@@ -0,0 +1,91 @@
+<?php
+require '../config/private.php';
+#
+# Set up the template for the curl cmd
+$template = 'curl -H "User-Agent: %s" -H "Content-Type: application/json" -u "%s" "https://api.getdrip.com/v2/%s/broadcasts?status=%s&page=%s"';
+#
+#
+for ($page_number=1; $page_number < MAX_ITERATIONS; $page_number++) {
+    #
+    # Inject user specific vars into cmd
+    $cmd = sprintf($template
+        , APP_NAME
+        , API_KEY
+        , ACCOUNT_ID
+        , STATUS_FILTER
+        , $page_number
+    );
+    #
+    # Grab the output
+    $json = shell_exec($cmd);
+    #
+    # Convert json to php array so we can work with it
+    $data = json_decode($json);
+    // print_r($data);die;
+    #
+    # Exit if there are no broadcasts
+    if (count($data->broadcasts) == 0) {
+        break;
+    }
+    #
+    # Loop through outputting md files to web dir
+    foreach ($data->broadcasts as $broadcast) {
+        #
+        # Subject will be used as title in the document; format it as you like here
+        $title = $broadcast->subject;
+        #
+        # Build a filename using the id and optionally filesafe subject
+        $name = str_pad($broadcast->id, 10, '0', STR_PAD_LEFT);
+        #
+        #
+        if (APPEND_SUBJECT_TO_FILENAME) {
+            #
+            # Create a filesystem safe subject
+            $filesafe_subject = $broadcast->subject;
+            $filesafe_subject = str_replace(' ', '_', (trim($filesafe_subject)));
+            $filesafe_subject = preg_replace( "/[^a-zA-Z0-9_\[\]]/", "", $filesafe_subject);
+            $name.= '_' . $filesafe_subject;
+        }
+        $filename = $_SERVER['DOCUMENT_ROOT'] . '/output/' . $name . '.md';
+        // echo $filename . "\n"; continue;
+        #
+        # Format the date if you like
+        if (isset($broadcast->send_at)) {
+            $date = 'Sent on ' . date('F jS, Y', strtotime($broadcast->send_at));
+        } else {
+            $date = 'Written on ' . date('F jS, Y', strtotime($broadcast->created_at));
+        }
+        #
+        # Clean up the body a bit
+        if (!empty(DELETE_EVERYTHING_AFTER_STRING)) {
+            list($body, $junk) = explode(DELETE_EVERYTHING_AFTER_STRING, $broadcast->html_body, 2);
+        } else {
+            $body = $broadcast->html_body;
+        }
+        $body = str_ireplace(array('<p>', '</p>', '<div>', '</div>', '<ul>', '</ul>', '<ol>', '</ol>', '</li>', '</h2>', '</h3>', '</h4>', '<br />', '&#9;'), '', $body);
+        $body = str_ireplace(array('<strong>', '</strong>'), '**', $body);
+        $body = str_ireplace(array('<em>', '</em>'), '_', $body);
+        $body = str_ireplace('<h1>', '# ', $body);
+        $body = str_ireplace('<h2>', '## ', $body);
+        $body = str_ireplace('<h3>', '### ', $body);
+        $body = str_ireplace('<h4>', '#### ', $body);
+        $body = str_ireplace('<li>', '* ', $body);
+        $body = str_ireplace('&#8212;', '—', $body);
+        $body = str_ireplace('&nbsp;', ' ', $body);
+        $body = trim($body);
+        #
+        # Assemble the file contents
+        $contents = $title . "\n";
+        $contents.= "====\n\n";
+        $contents.= $date . "\n\n";
+        $contents.= $body . "\n\n";
+        $contents.= PAGE_FOOTER;
+        #
+        # Output content to file
+        file_put_contents($filename, $contents);
+    }
+    #
+    # Pause to avoid rate limiting
+    sleep(1);
+}
+die("done\n");
